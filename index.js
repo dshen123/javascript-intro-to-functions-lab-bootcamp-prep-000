f6bc10dcc414699aad7cccd9fb7014b27a2f5972
law123school45@gmail.com <law123school45@gmail.com>
2018-07-24 00:33:00 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,26 @@
+'HELLO!'.toLowerCase()
+function whisper(string) {
+  return string.toLowerCase()}
+
+'Hello!'.toUpperCase()
+function shout(string) {
+  return string.toUpperCase()}
+
+'Hello!'.toUpperCase()
+ function logShout(string) {
+  console.log(string.toUpperCase())
+}
+'HELLO!'.toLowerCase()
+  function logWhisper(string){
+    console.log(string.toLowerCase())
+  }
+  
+function sayHiToGrandma(string) 
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  } else if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+  else 
+    return ("I can't hear you!")
+  
