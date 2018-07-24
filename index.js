d3cbcee732722c1fb619c4946bf4d1caeaf88e03
law123school45@gmail.com <law123school45@gmail.com>
2018-07-24 05:16:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,12 +15,6 @@ function shout(string) {
     console.log(string.toLowerCase())
   }
   
-function sayHiToGrandma(string) 
-  if (string.toUpperCase() === string) {
+if (string.toUpperCase() === string) {
     return "YES INDEED!"
-  } else if (string === "I love you, Grandma.") {
-    return "I love you, too."
   }
-  else 
-    return ("I can't hear you!")
-  
